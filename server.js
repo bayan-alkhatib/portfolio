f5b1d0888b1e903a2d3887301b6337702c88f76a
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-04-22 17:32:41 +0300

    server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict'
+const express=require('express');
+
+const server = express();
+
+const PORT =process.env.PORT || 2500;
+
+server.listen(PORT,()=>{
+    console.log(`this is port ${PORT}`);
+});
+
+server.get('/start',(req,res)=>{
+   res.send('Hello');
+})
+
